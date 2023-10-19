205ed7bb3e7eeec45aeb75848fe59c87295b2ca7
devBuzz142 <devbuzz142@gmail.com>
2023-10-19 20:43:39 +0900

    feat: temp homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import MakeDesignPage from './pages/MakeDesignPage/MakeDesignPage';
 import MakeInfoPage from './pages/MakeInfoPage/MakeInfoPage';
 import PageTemplate from './pages/PageTemplate/PageTemplate';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
     <PageTemplate>
       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/make" element={<MakeInfoPage />} />
         <Route path="/make/info" element={<MakeInfoPage />} />
         <Route path="/make/design" element={<MakeDesignPage />} />
       </Routes>
