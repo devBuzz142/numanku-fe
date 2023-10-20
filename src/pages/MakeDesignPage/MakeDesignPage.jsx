f55777b4b0fa4ca203f1a1ba18a3b12fe7d6099c
devBuzz142 <devbuzz142@gmail.com>
2023-10-20 23:01:06 +0900

    feat: button 스타일링

--- src/pages/MakeDesignPage/MakeDesignPage.jsx
+++ src/pages/MakeDesignPage/MakeDesignPage.jsx
@@ -1,8 +1,29 @@
+import styled from '@emotion/styled';
+import { useNavigate } from 'react-router-dom';
+
+const Container = styled.div`
+  button {
+    padding: 8px;
+    border: 2px solid black;
+    background-color: white;
+  }
+`;
+
 const MakeDesignPage = () => {
+  const navigate = useNavigate();
+
+  const handleSubmitClick = () => {
+    navigate('/make/qr');
+  };
+
   return (
-    <div>
+    <Container>
       <h1>MakeDesignpage</h1>
-    </div>
+      <br />
+      <button type="submit" onClick={handleSubmitClick}>
+        Design Submit
+      </button>
+    </Container>
   );
 };
 
