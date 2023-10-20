247cdc25f0ed07d8505efc177debd6f86f41a5c6
devBuzz142 <devbuzz142@gmail.com>
2023-10-20 23:46:50 +0900

    feat: MakeDesignPage 기초 구현

--- src/pages/MakeDesignPage/MakeDesignPage.jsx
+++ src/pages/MakeDesignPage/MakeDesignPage.jsx
@@ -9,6 +9,15 @@ const Container = styled.div`
   }
 `;
 
+const KukiCanvas = styled.div`
+  width: 296px;
+  height: 296px;
+
+  margin-left: 32px;
+
+  background-color: white;
+`;
+
 const MakeDesignPage = () => {
   const navigate = useNavigate();
 
@@ -16,9 +25,19 @@ const MakeDesignPage = () => {
     navigate('/make/qr');
   };
 
+  // 사용자애게 guide comment가 꽤 필요할 듯.
+
   return (
     <Container>
-      <h1>MakeDesignpage</h1>
+      <h1>MakeDesignPage</h1>
+      <br />
+      <KukiCanvas />
+      <br />
+      <div>
+        <div>Outter Tab</div>
+        <div>Outter_Color Tab</div>
+        <div>Inner Tab</div>
+      </div>
       <br />
       <button type="submit" onClick={handleSubmitClick}>
         Design Submit
