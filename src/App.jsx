ce2d4036452814ffd291c32a91756fe3ec8df4ba
deeyo <130573068+jeehyojeon@users.noreply.github.com>
2023-10-22 19:28:17 +0900

    feat: makedesign page 완성

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import React from "react";
+
 import PageTemplate from "./pages/PageTemplate/PageTemplate";
+import ViewDesignPage from "./pages/PageTemplate/ViewDesignPage/ViewDesignPage";
+import ViewMainPage from "./pages/PageTemplate/ViewMainPage/ViewMainPage";
+
+import {Routes, Route } from "react-router-dom";
+
+
 
 function App() {
   return (
     <PageTemplate>
-      
+        <Routes>
+          <Route path="/view" element={<ViewMainPage />} />
+          <Route path="/view/design" element={<ViewDesignPage />} />
+        </Routes>
     </PageTemplate>
   );
 }
