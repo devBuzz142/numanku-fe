7c96117978a3c3b097d1379560a7837751c60548
deeyo <130573068+jeehyojeon@users.noreply.github.com>
2023-11-07 16:08:07 +0900

    feat: view-complete-page 라우팅 구현까지 완료

--- src/pages/ViewCompletePage/ViewCompletePage.jsx
+++ src/pages/ViewCompletePage/ViewCompletePage.jsx
@@ -1,47 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
 import * as S from './ViewCompletePage.style';
+import { useNavigate } from 'react-router-dom';
 
 import TEMP_KUKI from '../../assets/kuki1.svg';
-import ViewMainPage from '../ViewMainPage/ViewMainPage';
 
 const ViewCompletePage = () => {
-  const [showOnboarding, setShowOnboarding] = useState(true);
-  const [redirectToview, setRedirectToView] = useState(false);
-
+  const navigate = useNavigate();
+  
   useEffect(() => {
-    const onboardingDuration = 5000;
-
-    const timeoutId = setTimeout(() => {
-      setShowOnboarding(false);
-      setRedirectToView(true);
-    }, onboardingDuration);
-
+    const timer = setTimeout(() => {
+      navigate('/view');
+    }, 3000);
     return () => {
-      clearTimeout(timeoutId);
-    };
-  }, []);
-
-  if (redirectToview) {
-    return (
-      <Routes>
-        <Route path="/view" element={<ViewMainPage />} />
-      </Routes>
-    )
-  }
+      clearTimeout(timer);
+    }
+  }, [navigate]);
 
   return (
-    <div>
-      {showOnboarding && (
-      <S.ViewCompletePage>
-        <S.HeaderContainer>
-          쿠키 선물 완료!
-        </S.HeaderContainer>
-        <S.KukiContainer image={TEMP_KUKI} />
-      </S.ViewCompletePage>
-      )}
-    </div>
-    
+    <S.ViewCompletePage>
+      <S.HeaderContainer>
+        쿠키 선물 완료!
+      </S.HeaderContainer>
+      <S.KukiContainer image={TEMP_KUKI} />
+    </S.ViewCompletePage>
   )
 }
 
