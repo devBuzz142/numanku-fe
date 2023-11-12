38917f093bc913b8ff53b61e8b87eaaf108c4595
devBuzz142 <devbuzz142@gmail.com>
2023-11-12 15:38:42 +0900

    feat: MakeDesignPage 레이아웃

--- src/pages/MakeDesignPage/MakeDesignPage.jsx
+++ src/pages/MakeDesignPage/MakeDesignPage.jsx
@@ -1,119 +1,42 @@
-import styled from '@emotion/styled';
-import { useNavigate } from 'react-router-dom';
-import Tab, { useTab } from '../../components/Tab/Tab';
-import { useEffect, useRef, useState } from 'react';
-
-const Container = styled.div`
-  button {
-    padding: 8px;
-    border: 2px solid black;
-    background-color: white;
-  }
-`;
-
-const KukiCanvas = styled.canvas`
-  width: 296px;
-  height: 296px;
-
-  margin-left: 32px;
-
-  background-color: white;
-`;
-
-const PALLETE = [
-  '#000000',
-  '#FF0000',
-  '#00FF00',
-  '#0000FF',
-  '#FFFF00',
-  '#FF00FF',
-];
-
-const PalleteContainer = styled.div`
-  display: flex;
-`;
-
-const PalleteItem = styled.div`
-  margin: 16px;
-
-  width: 32px;
-  height: 32px;
-  background-color: ${(props) => props.color};
-`;
+import * as S from './MakeDesignPage.style';
+import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 
 const MakeDesignPage = () => {
-  const navigate = useNavigate();
-
-  const [selected, setSelected] = useTab();
-
-  const canvasRef = useRef(null);
-  const [canvasCtx, setCanvasCtx] = useState(null);
-  const [isPainting, setIsPainting] = useState(false);
-
-  const [colorIdx, setColorIdx] = useState(0);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    setCanvasCtx(ctx);
-  }, []);
-
-  useEffect(() => {
-    if (canvasCtx) {
-      canvasCtx.strokeStyle = PALLETE[colorIdx];
-    }
-  }, [canvasCtx, colorIdx]);
-
-  const handleDraw = (e) => {
-    const { offsetX, offsetY } = e.nativeEvent;
-
-    if (!isPainting) {
-      canvasCtx.beginPath();
-      canvasCtx.moveTo(offsetX, offsetY);
-    } else {
-      canvasCtx.lineTo(offsetX, offsetY);
-      canvasCtx.stroke();
-    }
-  };
-
-  const handleSubmitClick = () => {
-    navigate('/make/qr');
-  };
-
-  // 사용자애게 guide comment가 꽤 필요할 듯.
-
   return (
-    <Container>
-      <h1>MakeDesignPage</h1>
-      <br />
-      <KukiCanvas
-        ref={canvasRef}
-        onMouseDown={() => setIsPainting(true)}
-        onMouseMove={handleDraw}
-        onMouseUp={() => setIsPainting(false)}
-        onMouseLeave={() => setIsPainting(false)}
-      />
-      <br />
-      <PalleteContainer>
-        {PALLETE.map((color, idx) => (
-          <PalleteItem
-            key={`pallete-${idx}`}
-            color={color}
-            onClick={() => setColorIdx(idx)}
-          />
-        ))}
-      </PalleteContainer>
-      <br />
-      <Tab
-        items={['Outter', 'Outter_Color', 'Inner']}
-        selected={selected}
-        setSelected={setSelected}
-      />
-      <br />
-      <button type="submit" onClick={handleSubmitClick}>
-        Design Submit
-      </button>
-    </Container>
+    <S.MakeDesignPage>
+      <S.HeaderWrapper>
+        <Header width={368} height={284} color={'rgba(78, 65, 59, 0.40)'}>
+          <div>자유롭게</div>
+          <div>그려주세요!</div>
+        </Header>
+      </S.HeaderWrapper>
+      <S.ControllerContainer>
+        <S.ToolWrapper>
+          <S.Tool>펜</S.Tool>
+          <S.Tool>지우개</S.Tool>
+        </S.ToolWrapper>
+        <S.Controller>
+          <S.MenuTab>
+            <div>쿠키 모양</div>
+            <div>쿠키 그림</div>
+            <div>그림 색상</div>
+          </S.MenuTab>
+          <S.DrawingTab>
+            <div>쿠키 그림</div>
+            <div>쿠키 그림</div>
+            <div>쿠키 그림</div>
+          </S.DrawingTab>
+          <S.CollorPalleteWrapper>
+            <div>ColorPalette</div>
+            <div>ColorPalette</div>
+          </S.CollorPalleteWrapper>
+          <S.ButtonWrapper>
+            <Button>포장하기</Button>
+          </S.ButtonWrapper>{' '}
+        </S.Controller>
+      </S.ControllerContainer>
+    </S.MakeDesignPage>
   );
 };
 
---
+++ src/pages/MakeDesignPage/MakeDesignPage.style.jsx
@@ -0,0 +1,73 @@
+import styled from '@emotion/styled';
+
+export const MakeDesignPage = styled.div`
+  top: 44px;
+  left: 50%;
+  transform: translateX(-50%);
+
+  width: 710px;
+  height: 1540px;
+
+  border-radius: 30px;
+  border: 5px solid #4e413b;
+`;
+
+export const HeaderWrapper = styled.div`
+  top: 25%;
+  left: 50%;
+  transform: translate(-50%, 0);
+
+  width: 368px;
+`;
+
+export const ControllerContainer = styled.div`
+  bottom: 0;
+
+  width: 710px;
+`;
+
+export const ToolWrapper = styled.div`
+  width: 100%;
+  padding-right: 16px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+`;
+
+export const Tool = styled.div`
+  width: 96px;
+  height: 96px;
+
+  border: none;
+  border-radius: 50%;
+
+  background-color: white;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 28px;
+`;
+
+export const Controller = styled.div`
+  border-radius: 30px;
+  background: #4e413b;
+`;
+
+export const MenuTab = styled.div`
+  display: flex;
+`;
+
+export const DrawingTab = styled.div`
+  display: flex;
+`;
+
+export const ColorTab = styled.div`
+  display: flex;
+`;
+
+export const CollorPalleteWrapper = styled.div``;
+
+export const ButtonWrapper = styled.div``;
