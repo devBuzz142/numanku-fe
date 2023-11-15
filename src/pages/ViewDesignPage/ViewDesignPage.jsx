f83787804cf7f47dd16b05e169e709dc57926e84
devBuzz142 <devbuzz142@gmail.com>
2023-11-15 17:47:38 +0900

    feat: ViewDesignPage 구현

--- src/pages/ViewDesignPage/ViewDesignPage.jsx
+++ src/pages/ViewDesignPage/ViewDesignPage.jsx
@@ -1,71 +1,111 @@
-import styled from '@emotion/styled';
+import * as S from './ViewDesignPage.style';
+import Header from '../../components/Header/Header';
+import Icon from '../../components/Icon/Icon';
+import Button from '../../components/Button/Button';
+
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const Container = styled.div`
-  input {
-    background-color: white;
-  }
-
-  button {
-    border: 2px solid black;
-    background-color: white;
-  }
-`;
+import { KUKI_IMAGES } from '../../commons/dummy';
 
 const ViewDesignPage = () => {
   const navigate = useNavigate();
-
   const [kuki, setKuki] = useState({
-    content: '',
-    design: '',
-    name: '',
-    open: '',
+    outterImage: 0,
+    innerImage: 0,
   });
 
-  const handleKukiChange = (e) => {
-    let { id, value } = e.target;
+  const [activeTab, setActiveTab] = useState({
+    menu: 'outter', // 'outter' | 'inner'
+    outter: 0, // 0 ~ 2
+    inner: 0, // 0 ~ 2
+  });
 
-    setKuki({
-      ...kuki,
-      [id]: value,
-    });
-  };
-
-  const handleSubmitClick = () => {
-    navigate('/view/write');
+  const handleTabChange = (name, value) => {
+    setActiveTab({ ...activeTab, [name]: value });
   };
 
   return (
-    <Container>
-      <h1>ViewDesignPage</h1>
-      <form>
-        <label htmlFor="content">content</label>
-        <input type="text" id="content" onChange={handleKukiChange} />
-        <br />
-
-        <label htmlFor="design">design</label>
-        <select onChange={(e) => handleKukiChange(e)}>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-        </select>
-        <br />
-
-        <label htmlFor="name">name</label>
-        <input type="text" id="name" onChange={handleKukiChange} />
-        <input type="text" id="content" onChange={handleKukiChange} />
-        <br />
-        <label htmlFor="open">public or private</label>
-        <select onChange={(e) => handleKukiChange(e)}>
-          <option value="public">공개</option>
-          <option value="private">비공개</option>
-        </select>
-      </form>
-      <button type="button" onClick={handleSubmitClick}>
-        submit
-      </button>
-    </Container>
+    <S.ViewDesignPage>
+      <S.HeaderContainer>
+        <S.IconWrapper onClick={() => navigate('/view')}>
+          <Icon name="BACK_FILL" width={46} />
+        </S.IconWrapper>
+        <Header width={346}>{'예쁘게\n꾸며주세요!'}</Header>
+      </S.HeaderContainer>
+      <S.KukiContainer>
+        <S.KukiWrapper>
+          <S.Kuki
+            outter={KUKI_IMAGES.outter[activeTab.outter]}
+            inner={KUKI_IMAGES.inner[activeTab.inner]}
+            menu={activeTab.menu}
+          />
+        </S.KukiWrapper>
+      </S.KukiContainer>
+      <S.ControllerContainer>
+        <S.MenuWrapper>
+          <S.MenuTab
+            selected={activeTab.menu === 'outter'}
+            onClick={() => handleTabChange('menu', 'outter')}
+          >
+            쿠키 모양
+          </S.MenuTab>
+          <S.MenuTab
+            selected={activeTab.menu === 'inner'}
+            onClick={() => handleTabChange('menu', 'inner')}
+          >
+            쿠키 그림
+          </S.MenuTab>
+        </S.MenuWrapper>
+        <S.Controller>
+          <S.DrawingContainer selected={activeTab.menu === 'outter'}>
+            <S.DrawingWrapper
+              selected={activeTab.outter === 0}
+              onClick={() => handleTabChange('outter', 0)}
+            >
+              쿠키 모양1
+            </S.DrawingWrapper>
+            <S.DrawingWrapper
+              selected={activeTab.outter === 1}
+              onClick={() => handleTabChange('outter', 1)}
+            >
+              쿠키 모양2
+            </S.DrawingWrapper>
+            <S.DrawingWrapper
+              selected={activeTab.outter === 2}
+              onClick={() => handleTabChange('outter', 2)}
+            >
+              쿠키 모양3
+            </S.DrawingWrapper>
+          </S.DrawingContainer>
+          <S.DrawingContainer selected={activeTab.menu === 'inner'}>
+            <S.DrawingWrapper
+              selected={activeTab.inner === 0}
+              onClick={() => handleTabChange('inner', 0)}
+            >
+              쿠키 그림1
+            </S.DrawingWrapper>
+            <S.DrawingWrapper
+              selected={activeTab.inner === 1}
+              onClick={() => handleTabChange('inner', 1)}
+            >
+              쿠키 그림2
+            </S.DrawingWrapper>
+            <S.DrawingWrapper
+              selected={activeTab.inner === 2}
+              onClick={() => handleTabChange('inner', 2)}
+            >
+              쿠키 그림3
+            </S.DrawingWrapper>
+          </S.DrawingContainer>
+          <S.ButtonWrapper>
+            <Button width={626} onClick={() => navigate('/view/write')}>
+              오븐에 넣기
+            </Button>
+          </S.ButtonWrapper>
+        </S.Controller>
+      </S.ControllerContainer>
+    </S.ViewDesignPage>
   );
 };
 
