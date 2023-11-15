c4da2373547ec62c404e7002ce50e21719d46596
devBuzz142 <devbuzz142@gmail.com>
2023-11-15 21:51:35 +0900

    feat: ViewComplete 수정

--- src/pages/ViewCompletePage/ViewCompletePage.jsx
+++ src/pages/ViewCompletePage/ViewCompletePage.jsx
@@ -1,29 +1,28 @@
 import React, { useEffect } from 'react';
 import * as S from './ViewCompletePage.style';
+
 import { useNavigate } from 'react-router-dom';
 
-import TEMP_KUKI from '../../assets/kuki1.svg';
+import { TEMP_KUKIES } from '../../commons/dummy';
 
 const ViewCompletePage = () => {
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     const timer = setTimeout(() => {
       navigate('/view');
     }, 3000);
     return () => {
       clearTimeout(timer);
-    }
+    };
   }, [navigate]);
 
   return (
     <S.ViewCompletePage>
-      <S.HeaderContainer>
-        쿠키 선물 완료!
-      </S.HeaderContainer>
-      <S.KukiContainer image={TEMP_KUKI} />
+      <S.HeaderContainer>쿠키 선물 완료!</S.HeaderContainer>
+      <S.KukiContainer>kuki</S.KukiContainer>
     </S.ViewCompletePage>
-  )
-}
+  );
+};
 
 export default ViewCompletePage;
