685614c3072c9b566b1f9e2536054ea665760f24
devBuzz142 <devbuzz142@gmail.com>
2023-11-18 05:02:28 +0900

    feat: 로그인 페이지 수정

--- src/pages/ViewLoginPage/ViewLoginPage.jsx
+++ src/pages/ViewLoginPage/ViewLoginPage.jsx
@@ -17,23 +17,40 @@ const ViewLoginPage = () => {
 
   const handleLoginChange = (e) => {
     let { id, value } = e.target;
+
     setLogin({
       ...login,
       [id]: value,
     });
   };
 
+  const handleLoginClick = () => {
+    if (login.name === ' ' || login.password === ' ') {
+      alert('이름과 비밀번호를 입력해주세요.');
+
+      return;
+    }
+
+    if (login.name.length > 12 || login.password.length > 12) {
+      alert('이름과 비밀번호는 최대 12글자까지 입력 가능합니다.');
+
+      return;
+    }
+
+    navigate('/view');
+  };
+
   return (
     <S.ViewLoginPageContainer>
       <S.HeaderContainer>
         <S.HeaderWrapper>
-          <Header width={286}>로그인</Header>
+          <Header>로그인</Header>
         </S.HeaderWrapper>
       </S.HeaderContainer>
       <S.MainContainer>
         <S.Form>
           <Input
-            width={550}
+            width={580}
             label="이름"
             type="text"
             id="name"
@@ -41,9 +58,10 @@ const ViewLoginPage = () => {
             backgroundColor={'#D9D9D9'}
             noBorder
             labelAlign="center"
+            placeholder={'최대 12글자까지 입력 가능합니다.'}
           />
           <Input
-            width={550}
+            width={580}
             label="비밀번호"
             type="text"
             id="password"
@@ -51,9 +69,10 @@ const ViewLoginPage = () => {
             backgroundColor={'#D9D9D9'}
             noBorder
             labelAlign="center"
+            placeholder={'최대 12글자까지 입력 가능합니다.'}
           />
           <Input
-            width={550}
+            width={580}
             label="제작진 비밀번호"
             type="text"
             id="makerCode"
@@ -61,10 +80,11 @@ const ViewLoginPage = () => {
             backgroundColor={'#D9D9D9'}
             noBorder
             labelAlign="center"
+            placeholder={'공연 제작진일 경우에만 입력해주세요.'}
           />
         </S.Form>
         <S.ButtonWrapper>
-          <Button type="button" onClick={() => navigate('/view')}>
+          <Button type="button" onClick={handleLoginClick}>
             계정 생성
           </Button>
         </S.ButtonWrapper>
